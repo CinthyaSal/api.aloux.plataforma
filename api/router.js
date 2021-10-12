653323511750ae4296d92ad0129c773b7d9aa76a
AlbertoGarciaV <alberto.garcia@aloux.mx>
2021-10-11 21:42:45 -0500

    fix

--- api/router.js
+++ api/router.js
@@ -6,7 +6,7 @@ router.use(fileupload());
 
 const admin    = require('./v0/controllers/admin.js')
 const article  = require('./v0/controllers/article.js')
-const worker   = require('./v0/controllers/worker.js')
+const worker   = require('./v0/controllers/worker')
 const stage    = require('./v0/controllers/stage.js')
 const customer = require('./v0/controllers/customer.js')
 const project  = require('./v0/controllers/project.js')
@@ -43,12 +43,20 @@ router.delete('/v0/admin/article/:id',      authAdmin, article.delArticle)
 
 //Customer
 router.post('/v0/customer',                 authAdmin, customer.create);
-//router.put('/v0/customer/edit',             authAdmin, customer.edit);
-//router.put('/v0/customer/change/state',     authAdmin, customer.change.state);// verificar
-//router.delete('/v0/customer/delete/:id',    authAdmin, customer.delete);
-//router.put('/v0/customer/asignProyect',     authAdmin, customer.asignProyect);
-//router.get('/v0/customer/proyectList',      authAdmin, customer.proyectList);
-//router.put('/v0/customer/change/:id',       authAdmin, customer.change.logo);
+router.put('/v0/customer/edit/:id',             authAdmin, customer.update);
+// router.put('/v0/customer/updateState/:id',   authAdmin, customer.updateState);
+// router.delete('/v0/customer/delete/:id',    authAdmin, customer.delete);
+// router.put('/v0/customer/asignProyect/:id',  authAdmin, customer.asignProyect);
+// router.get('/v0/customer/proyectList/:id',   authAdmin, customer.proyectList);
+// router.put('/v0/customer/updateLogo/:id',       authAdmin, customer.updateLogo);
+
+// //Payments
+// router.post('/v0/payments',                 authAdmin, customer.create);
+// router.put('/v0/customer/update/:id',       authAdmin, customer.update);
+// router.delete('/v0/customer/delete/:id',    authAdmin, customer.delete);
+// router.put('/v0/customer/asign/:id',        authAdmin, customer.asign);
+// router.put('/v0/customer/paid/:id',         authAdmin, customer.paid);
+// router.put('/v0/customer/revert/:id',       authAdmin, customer.revert);
 
 //Worker
 router.post('/v0/admin/worker',             authAdmin, worker.create)
