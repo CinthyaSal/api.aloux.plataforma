83b4955db001002672679ab513f5bcdb6a36346a
AlbertoGarciaV <alberto.garcia@aloux.mx>
2021-10-14 13:03:46 -0500

    training

--- api/v0/models/Training.js
+++ api/v0/models/Training.js
@@ -4,8 +4,9 @@ const config = require("../../config")
 const trainingSchema = mongoose.Schema({
     name:               { type: String, required: true, trim: true },
     email:              { type: String, required: true, trim: true },  
-    phone:              { type: Number, required: true, trim: true },  
-    comment:            { type: String, required: true, trim: true }
+    school:             { type: Number, required: true, trim: true },  
+    program:            { type: String, required: true, trim: true },
+    phone:              { type: Number, required: true, trim: true }
 })
   
 const Training = mongoose.model("Training", trainingSchema);
